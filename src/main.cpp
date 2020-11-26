e123766f2adf4c17904a2c03c797fcdc47a9fc9d
Kevin Kellar <kellar.kevin@gmail.com>
2020-11-25 20:12:34 -0800

    does this multicolored dubs work?

--- src/main.cpp
+++ src/main.cpp
@@ -739,7 +739,7 @@ public:
 		glGenBuffers(1, &owie_GPU_id);
 		glBindBuffer(GL_SHADER_STORAGE_BUFFER, owie_GPU_id);
 		glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(OWIE), &owiemem, GL_DYNAMIC_COPY);
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, owie_GPU_id);
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, owie_GPU_id);
 		glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0); // unbind
 	}
 
@@ -830,7 +830,7 @@ public:
 		glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 
 		block_index = glGetProgramResourceIndex(computeProgram, GL_SHADER_STORAGE_BLOCK, "shader_data");
-		GLuint ssbo_binding_point_index = 2;
+		GLuint ssbo_binding_point_index = 0;
 		glShaderStorageBlockBinding(computeProgram, block_index, ssbo_binding_point_index);
 		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, ssbo_GPU_id);
 		glUseProgram(computeProgram);
